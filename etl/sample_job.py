36cc4f564496b5bdbe42c76c36aeb9b3833bf400
Debarun De <debarun.de@unisys.com>
2025-07-10 13:03:25 -0400

    lets try it with a samply etl script

---
+++ etl/sample_job.py
@@ -0,0 +1,20 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContest
+from pyspark.context import SparkContext
+
+#for testing locally, wrap in try/except
+try:
+    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    
+    #sample data
+    df = glueContext.spark_session.createDataFrame(
+        [('Alice',34), ('Bob',45)], ['name','age']
+    )
+    
+    df.show()
+    
+except Exception as e:
+    print(f"ETL script failed: {e}")
